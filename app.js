593c9c0816c7d214ef4b0d4185c895c59371bbc4
saulpereira <saul_pereira@hotmail.com>
2020-07-09 17:53:01 -0600

    Model with File

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.get404);
+//app.use(errorController.get404);
+app.use((req, res, next) => {
+    res.status(404).json('Not found')
+});
 
 app.listen(3000);
